0a9543c098696deb9cde549293bcb18f788475d6
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-11-17 22:40:01 +0200

    Edit StartUp Form

--- StartUpForm.Designer.cs
+++ StartUpForm.Designer.cs
@@ -32,6 +32,11 @@
             this.btn_Start = new System.Windows.Forms.Button();
             this.lbl_PrjName = new System.Windows.Forms.Label();
             this.lbl_info = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_Start
@@ -41,7 +46,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_Start.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.btn_Start.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_Start.Location = new System.Drawing.Point(277, 365);
+            this.btn_Start.Location = new System.Drawing.Point(277, 488);
             this.btn_Start.Name = "btn_Start";
             this.btn_Start.Size = new System.Drawing.Size(192, 61);
             this.btn_Start.TabIndex = 0;
@@ -68,19 +73,101 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_info.AutoSize = true;
-            this.lbl_info.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbl_info.Location = new System.Drawing.Point(110, 92);
+            this.lbl_info.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_info.Location = new System.Drawing.Point(113, 100);
             this.lbl_info.MaximumSize = new System.Drawing.Size(600, 400);
             this.lbl_info.Name = "lbl_info";
-            this.lbl_info.Size = new System.Drawing.Size(599, 242);
+            this.lbl_info.Size = new System.Drawing.Size(588, 54);
             this.lbl_info.TabIndex = 3;
-            this.lbl_info.Text = resources.GetString("lbl_info.Text");
+            this.lbl_info.Text = "# This application program is designed to aid the project Owners, or their repres" +
+    "entative (The Consultants/PMO), manage the process of closeout documents\' delive" +
+    "ry to the Owner.";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(113, 169);
+            this.label1.MaximumSize = new System.Drawing.Size(600, 400);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(600, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "# This Application is designed for construction projects, specially commercial pr" +
+    "ojects.";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(113, 202);
+            this.label2.MaximumSize = new System.Drawing.Size(600, 400);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(598, 54);
+            this.label2.TabIndex = 5;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(113, 271);
+            this.label3.MaximumSize = new System.Drawing.Size(600, 400);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(580, 36);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "# We strongly recommend to keep a retention from the contractor\'s payment for the" +
+    " delivery of closeout documents only, other than the traditional retention. ";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(113, 322);
+            this.label4.MaximumSize = new System.Drawing.Size(600, 400);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(570, 54);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "# This retention is to be set between 3-5% of the total contract price to form a " +
+    "real motivation to the contractor in the documents delivery process, otherwise t" +
+    "he traditional retention can be used.";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(113, 391);
+            this.label5.MaximumSize = new System.Drawing.Size(600, 400);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(565, 36);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "# Closeout documents delivery process must be planned for in the time schedule su" +
+    "bmitted by  the contractor at the project beginning.";
             // 
             // frm_StartUp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 573);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lbl_info);
             this.Controls.Add(this.lbl_PrjName);
             this.Controls.Add(this.btn_Start);
@@ -98,5 +185,10 @@
         private Button btn_Start;
         private Label lbl_PrjName;
         private Label lbl_info;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
